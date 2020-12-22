8876224c13758cf2f6ec501e914cbd321bbeeaf4
Oliver Gray <olivergray@live.co.uk>
2020-12-22 10:14:56 +0000

    Part 2.5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,70 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { getCanvasPosition } from './utils/formulas';
+import Canvas from './components/Canvas';
+import * as Auth0 from 'auth0-web';
+
+Auth0.configure({
+  domain: 'dev-orq300tb.eu.auth0.com',
+  clientID: '7suSQJsjUpK6zngGmI6eY4hAmaw58YVC',
+  redirectUri: 'http://localhost:3000/',
+  responseType: 'token id_token',
+  scope: 'openid profile manage:points',
+});
+
 
 class App extends Component {
+  componentDidMount() {
+    const self = this;
+
+    Auth0.handleAuthCallback();
+
+    Auth0.subscribe((auth) => {
+      console.log(auth);
+    });
+    setInterval(() => {
+        self.props.moveObjects(self.canvasMousePosition);
+    }, 10);
+    window.onresize = () => {
+      const cnv = document.getElementById('aliens-go-home-canvas');
+      cnv.style.width = `${window.innerWidth}px`;
+      cnv.style.height = `${window.innerHeight}px`;
+    };
+    window.onresize();
+  }
+
+  trackMouse(event) {
+    this.canvasMousePosition = getCanvasPosition(event);
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>{this.props.message}</h1>
-      </div>
+      <Canvas
+        angle={this.props.angle}
+        gameState={this.props.gameState}
+        startGame={this.props.startGame}
+        trackMouse={event => (this.trackMouse(event))}
+      />
     );
   }
 }
 
 App.propTypes = {
-  message: PropTypes.string.isRequired,
+  angle: PropTypes.number.isRequired,
+  gameState: PropTypes.shape({
+    started: PropTypes.bool.isRequired,
+    kills: PropTypes.number.isRequired,
+    lives: PropTypes.number.isRequired,
+    flyingObjects: PropTypes.arrayOf(PropTypes.shape({
+      position: PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired,
+      id: PropTypes.number.isRequired,
+    })).isRequired,
+  }).isRequired,
+  moveObjects: PropTypes.func.isRequired,
+  startGame: PropTypes.func.isRequired,
 };
 
 export default App;
